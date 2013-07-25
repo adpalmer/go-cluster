46625b1b242a6789d69158b4ceb0adc94cdf23a5
Alex Palmer <alexpalmer90@gmail.com>
2013-07-25 02:08:42 -0400

    Separated example into test directory

---
+++ example/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"github.com/adpalmer/go-cluster"
+	"math"
+)
+
+type Point struct {
+	x float64
+	y float64
+}
+
+func (p Point) String() string {
+	return fmt.Sprintf("(%f, %f)", p.x, p.y)
+}
+
+func euclideanDist(P1, P2 interface{}) float64 {
+	p1, p2 := P1.(Point), P2.(Point)
+	return math.Sqrt(math.Pow(p1.x-p2.x, 2) + math.Pow(p1.y-p2.y, 2))
+}
+
+func updateCluster(c kmeans.Cluster) interface{} {
+	tot := Point{0., 0.}
+	for i := 0; i < len(c.Entity); i++ {
+		tmp := c.Entity[i].(Point)
+		tot.x += tmp.x
+		tot.y += tmp.y
+	}
+	tot.x /= float64(len(c.Entity))
+	tot.y /= float64(len(c.Entity))
+	return tot
+}
+
+func main() {
+	pts := []Point{Point{1, 1}, Point{2., .999}, Point{1, .3}, Point{10, 15}, Point{20, 10}, Point{15, 15}}
+
+	// make it work with function
+	interfacePoints := make([]interface{}, len(pts))
+
+	for i, d := range pts {
+		interfacePoints[i] = d
+	}
+
+	km := kmeans.LloydsAlgo(euclideanDist, updateCluster)
+	clusterCenters, clusterMembers, _ := km(interfacePoints, 2, 10)
+	for i := 0; i < len(clusterCenters); i++ {
+		fmt.Printf("Cluster Center %v:\n", clusterCenters[i].(Point))
+		fmt.Println("\tmembers -> ", clusterMembers[i].Entity)
+	}
+}
--- kmeans.go
+++ kmeans.go
@@ -1,25 +1,16 @@
-//package kmeans
-package main
+package kmeans
 
 import (
-	"fmt"
 	"math"
 	"math/rand"
 	"time"
 )
 
-/*   return func(entities []interface{}, k, maxIters int) ([]interface{}, []int, error) {
-      numEntities := len(entities)
-      fmt.Println(numEntities)
-      return nil, nil, nil
-   }
-}*/
-
 type Cluster struct {
-	entity []interface{}
+	Entity []interface{}
 }
 
-func kmeans(calcDist func(p1, p2 interface{}) float64, updateClust func(Cluster) interface{}) func([]interface{}, int, int) ([]interface{}, []Cluster, error) {
+func LloydsAlgo(calcDist func(p1, p2 interface{}) float64, updateClust func(Cluster) interface{}) func([]interface{}, int, int) ([]interface{}, []Cluster, error) {
 	return func(entities []interface{}, k, maxIters int) ([]interface{}, []Cluster, error) {
 		numEntities := len(entities)
 
@@ -37,7 +28,7 @@ func kmeans(calcDist func(p1, p2 interface{}) float64, updateClust func(Cluster)
 		for i := 0; i < maxIters; i++ {
 			// setup clusters
 			for i := 0; i < k; i++ {
-				clusters[i].entity = make([]interface{}, 0, 10)
+				clusters[i].Entity = make([]interface{}, 0, 10)
 			}
 			curVariance := 0. // current variance
 			for j := 0; j < numEntities; j++ {
@@ -50,7 +41,7 @@ func kmeans(calcDist func(p1, p2 interface{}) float64, updateClust func(Cluster)
 						minDist, minPt = dist, l
 					}
 				}
-				clusters[minPt].entity = append(clusters[minPt].entity, entities[j])
+				clusters[minPt].Entity = append(clusters[minPt].Entity, entities[j])
 				curVariance += minDist
 			}
 
@@ -65,48 +56,6 @@ func kmeans(calcDist func(p1, p2 interface{}) float64, updateClust func(Cluster)
 			prevVariance = curVariance
 
 		}
-		fmt.Println(centers)
 		return centers, clusters, nil
 	}
-}
-
-type Point struct {
-	x float64
-	y float64
-}
-
-func euclideanDist(P1, P2 interface{}) float64 {
-	p1, p2 := P1.(Point), P2.(Point)
-	return math.Sqrt(math.Pow(p1.x-p2.x, 2) + math.Pow(p1.y-p2.y, 2))
-}
-
-func updateCluster(c Cluster) interface{} {
-	tot := Point{0., 0.}
-	for i := 0; i < len(c.entity); i++ {
-		tmp := c.entity[i].(Point)
-		tot.x += tmp.x
-		tot.y += tmp.y
-	}
-	tot.x /= float64(len(c.entity))
-	tot.y /= float64(len(c.entity))
-	return tot
-}
-
-func T(x []interface{}) {
-	fmt.Println(x)
-}
-
-func main() {
-	pts := []Point{Point{1, 1}, Point{2., .999}, Point{1, .3}, Point{10, 15}, Point{20, 10}, Point{15, 15}}
-
-	// make it work with function
-	interfacePoints := make([]interface{}, len(pts))
-
-	for i, d := range pts {
-		interfacePoints[i] = d
-	}
-	km := kmeans(euclideanDist, updateCluster)
-	fmt.Println(km(interfacePoints, 2, 10))
-	//kmeans(euclideanDist)
-	//fmt.Println(euclideanDist(Point{1,1}, Point{1.5,2}))
 }
